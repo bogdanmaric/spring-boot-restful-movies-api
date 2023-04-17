b888b1b4f9fd12da946344302b0e7605c6ffca15
SkyLightCode <67342565+SkyLightCode@users.noreply.github.com>
2023-04-17 18:37:10 +0200

    Update: add reference field for MovieService and modify test method allMovies in MovieController for proper testing

--- src/main/java/com/bogdan/movies/controller/MovieController.java
+++ src/main/java/com/bogdan/movies/controller/MovieController.java
@@ -1,15 +1,23 @@
 package com.bogdan.movies.controller;
 
+import com.bogdan.movies.model.Movie;
+import com.bogdan.movies.service.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("api/v1/movies")
 public class MovieController {
+    @Autowired
+    private MovieService movieService;
     @GetMapping
-    public String allMovies() {
-        return "allMovies";
+    public ResponseEntity<List<Movie>> allMovies() {
+        return new ResponseEntity<List<Movie>>(movieService.allMovies(), HttpStatus.OK);
     }
 }
