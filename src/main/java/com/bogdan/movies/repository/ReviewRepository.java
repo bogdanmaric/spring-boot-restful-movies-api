221440cb66ac7b2275f1af9d9bb597f64e85413c
SkyLightCode <67342565+SkyLightCode@users.noreply.github.com>
2023-04-18 12:18:55 +0200

    Add: ReviewRepository interface which extends MongoRepository

---
+++ src/main/java/com/bogdan/movies/repository/ReviewRepository.java
@@ -0,0 +1,10 @@
+package com.bogdan.movies.repository;
+
+import com.bogdan.movies.model.Review;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+}
