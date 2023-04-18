3c6941c55f0910377140ff0b63cfc2758c448324
SkyLightCode <67342565+SkyLightCode@users.noreply.github.com>
2023-04-18 13:59:18 +0200

    Add: createReview method in ReviewService for creating review using ReviewRepository reference field, and updating corresponding movie with review ID using MongoTemplate

--- src/main/java/com/bogdan/movies/service/ReviewService.java
+++ src/main/java/com/bogdan/movies/service/ReviewService.java
@@ -1,7 +1,30 @@
 package com.bogdan.movies.service;
 
+import com.bogdan.movies.model.Movie;
+import com.bogdan.movies.model.Review;
+import com.bogdan.movies.repository.ReviewRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ReviewService {
+    @Autowired
+    private ReviewRepository reviewRepository;
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    public Review createReview(String reviewBody, String imdbId) {
+        Review review = reviewRepository.insert(new Review(reviewBody));
+
+        mongoTemplate.update(Movie.class)
+                .matching(Criteria.where("imdbId").is(imdbId))
+                .apply(new Update().push("reviewIds").value(review))
+                .first();
+
+        return review;
+    }
 }
